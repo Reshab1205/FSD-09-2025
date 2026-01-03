56b8a9bd550318199caa3ef16b0b0ce956fe2b5d
Reshab Kumar <rkmodi12051998@gmail.com>
2026-01-03 19:06:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,87 @@
+// // var a = 10;
+// // console.log('Hii',a)
+// // var a = 12;
+// // a = 14;
+
+// // console.log(a)
+// // // var a;
+
+// // let b = 10;
+// // //  let b = 12;
+// //  b = 16;
+
+// const c = 12;
+// c = 15;
+// console.log(c)
+// const c = 13;
+
+// ECMAScript
+
+// const x = 12;
+// const y = 13;
+// const z = x + y;
+// console.log(z)
+
+// const a = 19;
+// const b = 19;
+
+// const c = parseInt(prompt('Enter the time: '))
+
+// const c = Date.now()
+
+// if(6 <= c && c<= 12 ) {
+//     console.log(`Good Morning`)
+// } else if(12 <= c && c <= 16) {
+//     console.log(`GD AfterNoon`)
+// } else if(16 < c && c <= 20) {
+//     console.log(`GD Evening`)
+// } else if((20 < c && c <= 24) || (1 <= c && c <6)) {
+//     console.log(`GD night`)
+// } else if(c > 24) {
+//     console.log(`Enter Valid Time`)
+// }
+
+// if(a >= b) {
+//     console.log(`A is Greater`)
+// } else if (b >= a) {
+//     console.log(`B is Greater`)
+// } else {
+//     console.log(`Both are Equal`)
+// }
+
+// const fname = 'reshab'
+// console.log(fname)
+
+// console.log(fname)
+
+// const data = ['Reshab', 'Kumar', 'Bihar']
+// // console.log(typeof(data))
+
+const obj = {
+  fname: "Reshab",
+  lanme: "Kumar",
+  city: "Vaishali",
+  state: "Bihar",
+};
+
+// console.log(obj.city);
+
+const facultyOfCodingThinker = [
+  {
+    faculty_id: 1,
+    faculty_name: "Reshab Kumar",
+    faculty_subject: "FSD",
+  },
+  {
+    faculty_id: 2,
+    faculty_name: "Ravi Kumar",
+    faculty_subject: "Java FSD",
+  },
+  {
+    faculty_id: 3,
+    faculty_name: "Baibhav Kumar",
+    faculty_subject: "HTML",
+  },
+];
+
+console.log(facultyOfCodingThinker[1].faculty_subject);
