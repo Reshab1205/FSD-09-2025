141ace246a40154cce4d52dcbe32d209d3f26800
Reshab Kumar <rkmodi12051998@gmail.com>
2026-01-31 19:00:51 +0530

    HTML DOM

---
+++ SetMethod.js
@@ -0,0 +1,16 @@
+// const data = new Set([1,2,3,4,5])
+
+// const data1 = new Set([6,7,8,9,4])
+
+// const c = data.difference(data1)
+
+// console.log(c)
+
+// let password = 'Reshab@1234'
+// let passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])(?=.{8,})/
+
+// const test = passwordRegex.test(password)
+// console.log(test)
+
+
+
--- index1.js
+++ index1.js
@@ -6,34 +6,56 @@
 // let b = 10;
 //  b = 19;
 
-// const c = 5;
-// // c = 10;
-// console.log(c)
+// // const c = 5;
+// // // c = 10;
+// // console.log(c)
 
 
 
-// console.log(a)
-// var a = 10;
+// // console.log(a)
+// // var a = 10;
 
-// function something() {
+// // function something() {
+// //     console.log('Hello')
+// // }
+
+// something()
+
+// // function something() {
+// //     console.log('BYe')
+// // }
+
+
+// var something = () => {
 //     console.log('Hello')
+
 // }
 
-something()
+// var something = () => {
+//     console.log('Hello')
 
-// function something() {
-//     console.log('BYe')
 // }
-
-
-var something = () => {
-    console.log('Hello')
-
-}
-
-var something = () => {
-    console.log('Hello')
-
-}
 
 // console.log(d)
+
+
+
+// console.log(Math.floor(100000 + Math.random() * 900000) )
+
+// console.log(parseInt(Math.random() * 900000))
+
+
+function randomString() {
+     alp = 'QWERTYUIOPLKJHGFDSAZXCVBNM'
+    alphabet = alp.charAt(Math.floor(Math.random() * alp.length))
+    alphabet1 = alp.charAt(Math.floor(Math.random() * alp.length))
+
+    return alphabet + alphabet1
+    
+}
+
+
+const num = parseInt(Math.random() * 9000)
+const data = randomString() + num
+
+console.log(data)
