7e829fbbc747bdaf986d7f74df88d30dad35b902
Reshab Kumar <rkmodi12051998@gmail.com>
2026-02-10 19:04:48 +0530

    form

--- Callback.js
+++ Callback.js
@@ -2,23 +2,17 @@
 //     console.log('Hello First')
 // }, 1000)
 
-
 // setTimeout(() => {
 //     console.log('Hello 2nd')
 // }, 2000)
-
 
 // setTimeout(() => {
 //     console.log('Hello 3rd')
 // }, 3000)
 
-
-
 // setTimeout(() => {
 //     console.log('Hello 4th')
 // }, 1000)
-
-
 
 // setTimeout(() => {
 //     console.log('Hello 5th')
@@ -41,16 +35,49 @@
 //     hello()
 // }, 2000);
 
+// setTimeout(() => {
+//     console.log('First')
+//     setTimeout(() => {
+//         console.log('2nd')
+//         setTimeout(() => {
+//             console.log('3rd')
+//         },2000)
+//     },1000)
+// }, 3000)
 
+// console.log('Hiii')
+
+// function exe() {
+//     setTimeout(() => {
+//         sayHello()
+//     }, 3000)
+//     console.log('2nd message')
+// }
+
+// function sayHello() {
+//     console.log('Hello')
+// }
+
+// setTimeout(() => {
+//     exe()
+//     console.log(`3rd message`)
+// }, 5000)
+
+// console.log('Bye')
+
+function exe() {
+  setTimeout(() => {
+    console.log(`first`);
+    setTimeout(() => {
+      console.log(`2nd`);
+      setTimeout(() => {
+        console.log(`3rd`);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}
 
 setTimeout(() => {
-    console.log('First')
-    setTimeout(() => {
-        console.log('2nd')
-        setTimeout(() => {
-            console.log('3rd')
-        },2000)
-    },1000)
-}, 3000)
-
-
+  exe();
+  console.log(`4th`)
+}, 1000);
