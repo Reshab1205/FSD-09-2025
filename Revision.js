6c7e31ef8304edee6009b0ad2dae6b05e7f64762
Reshab Kumar <rkmodi12051998@gmail.com>
2026-03-28 18:50:14 +0530

    todo

---
+++ Revision.js
@@ -0,0 +1,25 @@
+add()
+const sub = function () {
+    console.log('BYe')
+}
+
+
+sub = 10
+
+// let sub = () => {
+// console.log('ji')
+// }
+sub()
+// console.log(sub)
+
+function add() {
+    console.log('Hello')
+}
+
+function add() {
+    console.log('Hii')
+}
+
+
+
+// console.log(add)
--- TodoWithList/index.js
+++ TodoWithList/index.js
@@ -1,37 +1,51 @@
-const data = []
+const data = [];
 function addTodo() {
   const todo = document.getElementById("inp").value;
   if (todo.trim() === "") {
-    alert('Enter Todo')
-    return
+    alert("Enter Todo");
+    return;
   }
-  const li = document.createElement("li");
-  li.textContent = todo;
-  const editBtn = document.createElement('button')
-  editBtn.textContent = 'Edit'
-  editBtn.onclick = function () {
-    const newData = prompt('Enter the New Data')
-    if(newData.trim() !== "" ) {
-      li.firstChild.textContent = newData
-    }
-  }
-  const delBtn = document.createElement('button')
-  const pE = document.getElementById("ul");
-  console.log(li.firstChild.textContent)
-    delBtn.textContent = 'Delete'
-    delBtn.onclick = function () {
-      // li.remove()
-      pE.removeChild(li)
-    }
-    data.push(todo)
-  localStorage.setItem('data', JSON.stringify(data))
-  console.log(  JSON.parse(localStorage.getItem('data')))
-  li.appendChild(editBtn)
-  li.appendChild(delBtn)
-  pE.appendChild(li);
+  // const li = document.createElement("li");
+  // li.textContent = todo;
+  // const editBtn = document.createElement('button')
+  // editBtn.textContent = 'Edit'
+  // editBtn.onclick = function () {
+  //   const newData = prompt('Enter the New Data')
+  //   if(newData.trim() !== "" ) {
+  //     li.firstChild.textContent = newData
+  //   }
+  // }
+  // const delBtn = document.createElement('button')
+  // const pE = document.getElementById("ul");
+  //   delBtn.textContent = 'Delete'
+  //   delBtn.onclick = function () {
+  //     // li.remove()
+  //     pE.removeChild(li)
+  //   }
+  data.push(todo);
+  localStorage.setItem("data", JSON.stringify(data));
+  // const fetchData = JSON.parse(localStorage.getItem('data'));
+  // console.log(fetchData)
+  // fetchData.forEach((arr, index) => {
+  //   li.textContent = arr
+  // })
+  // li.textContent = data
+  // li.appendChild(editBtn)
+  // li.appendChild(delBtn)
+  // pE.appendChild(li);
   document.getElementById("inp").value = "";
-
-  function displayTodo() {
-
-  }
+  displayTodo()
 }
+
+function displayTodo() {
+  const pE = document.getElementById('ul')
+  const fetchData = JSON.parse(localStorage.getItem('data'))
+  fetchData.forEach((arr, index) => {
+    const li = document.createElement('li')
+    li.textContent = arr
+    pE.appendChild(li)
+  })
+  
+}
+
+window.onload = displayTodo;
