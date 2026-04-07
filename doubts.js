20b0df43211ae681332c40d14047fdedb9b25ab0
Reshab Kumar <rkmodi12051998@gmail.com>
2026-04-07 18:51:53 +0530

    form

---
+++ doubts.js
@@ -0,0 +1,54 @@
+// const obj = {
+//     fname: "Reshab",
+//     lname: "Kumar"
+// }
+
+// const arr = ["Reshab", "Kumar","Bhopal", 28]
+
+// obj.age = 28
+// Object.freeze(obj)
+// obj.fname = 'Chandan'
+// // obj['age'] = 28
+// console.log(obj)
+
+
+// const inp = '23+-56-12*2'
+
+// const result = eval(inp)
+// console.log(result)
+
+const student = {
+    name: "Reshab",
+    age:28,
+    course:'Full stack'
+}
+
+// delete student.age
+// console.log(student)
+// console.log(Object.entries(student))
+// // let count = 0
+// function checkProperty (p) {
+//     for(let i in student) {
+//         if( i === p) {
+//             console.log('Exist')
+//             return      
+//         }
+//     }
+//     console.log('NE')
+// }
+// checkProperty('city')
+    // console.log(count)
+// student['city'] = 29
+// console.log(student)
+
+// '2' + '3'
+
+// const data = 
+// const result = eval('2+4+6-3*5/3')
+// console.log(result)
+
+// const btn = document.getElementById('btn')
+// const btn = document.querySelector('#button')
+// btn.addEventListener('click', () => {
+
+// })
