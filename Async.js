560474f50565f08a24170b8fa6015e73e2155030
Reshab Kumar <rkmodi12051998@gmail.com>
2026-04-11 18:58:27 +0530

    callback

--- Async.js
+++ Async.js
@@ -1,18 +1,87 @@
-console.log("Hello");
+// console.log("Hello");
 
-setTimeout(() => {
-  console.log("Ist");
+// setTimeout(() => {
+//   console.log("Ist");
+//   setTimeout(() => {
+//     console.log("2nd");
+//     setTimeout(() => {
+//       console.log("3rd");
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+// setTimeout(() => {
+//   console.log("4th");
+// });
+
+// Promise.resolve().then(() => console.log("Promise"));
+// console.log("Bye");
+
+
+
+// function hello(callback) {
+//   console.log('Hello')
+//   callback()
+//   // hii()
+// }
+
+// function hii() {
+//   console.log('Hii')
+// }
+
+// function bye() {
+//   console.log('BYe')
+// }
+
+// hello(bye)
+
+
+// property 
+// method
+
+// let AtmType = ['VISA', 'MASTERCARD', 'RUPAY']
+
+// function fetchAtmType(callback, callback2) {
+//   let type = '';
+//   if(!AtmType.includes(type)) {
+//     return 'Card not supported'
+//   }
+//   callback(callback2)
+// }
+
+// function checkBalance(callback2) {
+// callback2()
+// }
+
+// verifyAtmPin()
+// function cashWithDrawl(callback2) {
+// callback2()
+// }
+
+// function verifyAtmPin() {
+
+// }
+// fetchAtmType(cashWithDrawl, verifyAtmPin)
+
+
+function getUser(callback) {
   setTimeout(() => {
-    console.log("2nd");
-    setTimeout(() => {
-      console.log("3rd");
-    }, 1000);
-  }, 1000);
-}, 1000);
+    callback({id:1, name: "Reshab"})
+  }, 1000)
+}
 
-setTimeout(() => {
-  console.log("4th");
-});
+function getOrders(user, callback) {
+  setTimeout(() => {
+    callback(["Order1", "Order2", "Order3"])
+  }, 1000)
+}
 
-Promise.resolve().then(() => console.log("Promise"));
-console.log("Bye");
+function getOrderDetails(callback) {
+  setTimeout(() => {
+
+  }, 1000)
+}
+
+getUser( function ({id: user}) {
+  getOrders()
+})
